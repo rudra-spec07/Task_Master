bba9bf1f83557a250abcf29e13ce409d16fddab0
rudrakashchoubey <rudrachoubey70@gmail.com>
2026-07-01 22:17:55 +0530

    updat

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,20 @@
+// import { Sequelize } from "sequelize";
+// import env from "./env.js";
+
+// const sequelize = new Sequelize(
+//   env.db.name,
+//   env.db.user,
+//   env.db.password,
+//   {
+//     host: env.db.host,
+//     port: env.db.port,
+//     dialect: "postgres",
+//     logging: false,
+//   }
+// );
+
+// export default sequelize;
+
 import { Sequelize } from "sequelize";
 import env from "./env.js";
 
@@ -10,6 +27,13 @@ const sequelize = new Sequelize(
     port: env.db.port,
     dialect: "postgres",
     logging: false,
+    // 👇 Yeh code check karega ki agar environment production hai toh hi SSL lagaye
+    dialectOptions: process.env.NODE_ENV === "production" ? {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    } : {} 
   }
 );
 
